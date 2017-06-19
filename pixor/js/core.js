055f54b86d77b80dd619e1a758d77e11835fc8a7
PatrickHortreiter <patrick.hoertreiter@gmx.de>
2017-06-19 17:47:40 +0200

    Lightbox Bootstrap Eingabefelder

--- pixor/js/core.js
+++ pixor/js/core.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
     var header = $('body');
 
     var backgrounds = new Array(
-        'url(/projectpiXOR/pixor/images/background.jpg)'
-        , 'url(/projectpiXOR/pixor/images/background2.jpg)'
-        , 'url(/projectpiXOR/pixor/images/background3.jpg)'
+        'url(/pixor/images/background.jpg)'
+        , 'url(/pixor/images/background2.jpg)'
+        , 'url(/pixor/images/background3.jpg)'
     );
 
     var current = 0;
