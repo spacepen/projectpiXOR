488a909a0ce50542c32bb0b2b181eedbd6ff73d1
unknown <patrick.hoertreiter@gmx.de>
2017-07-02 23:07:48 +0200

    almost counting answers XD

--- includes/answer.php
+++ includes/answer.php
@@ -12,5 +12,9 @@ $userId = $_SESSION['login_userid'];
 $fql = "INSERT INTO userquestion (selection, usersID, questionID) VALUES ('" . 1 . "', '" . 1 . "', '" . 1 . "');";
 $result = mysqli_query($pdo, $fql);
 
+$sql = "SELECT * FROM userquestion WHERE selection = 1";
+$result2= mysqli_query($pdo, $sql);
+echo mysqli_num_rows(result2);
+
 
 
--- includes/answerB.php
+++ includes/answerB.php
@@ -12,4 +12,8 @@ $userId = $_SESSION['login_userid'];
 $fql = "INSERT INTO userquestion (selection, usersID, questionID) VALUES ('" . 2 . "', '" . 1 . "', '" . 1 . "');";
 $result = mysqli_query($pdo, $fql);
 
+$sql = "SELECT * FROM userquestion WHERE selection = 2";
+$result2= mysqli_query($pdo, $sql);
+echo mysqli_num_rows(result2);
+
 
